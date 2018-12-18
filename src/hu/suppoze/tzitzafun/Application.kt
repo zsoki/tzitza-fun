e25d96435fc7384b3cafa9c1089f89a78c565b46
Zoltan Soki <v-zsoki@expedia.com>
2018-12-18 17:09:57 +0100

    [Add] Basic CSS

--- src/hu/suppoze/tzitzafun/Application.kt
+++ src/hu/suppoze/tzitzafun/Application.kt
@@ -2,18 +2,29 @@ package hu.suppoze.tzitzafun
 
 import hu.suppoze.tzitzafun.template.TzitzaMainTemplate
 import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.HttpsRedirect
 import io.ktor.html.insert
 import io.ktor.html.respondHtml
+import io.ktor.http.ContentType
 import io.ktor.network.tls.certificates.generateCertificate
+import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import kotlinx.css.CSSBuilder
+import kotlinx.css.Color
+import kotlinx.css.LinearDimension
+import kotlinx.css.body
+import kotlinx.css.margin
+import kotlinx.css.padding
+import kotlinx.css.px
 import kotlinx.html.head
+import kotlinx.html.link
 import kotlinx.html.title
 import java.io.File
 
@@ -36,12 +47,42 @@ fun Application.module() {
             call.respondHtml {
                 head {
                     title { +"TZITZA World" }
+                    link(rel = "stylesheet", href = "/style.css", type = "text/css")
                 }
                 insert(TzitzaMainTemplate()) {
                     welcomeMessage { +"=OwO= What's this?" }
                 }
             }
         }
+
+        get("/style.css") {
+            call.respondCss {
+                body {
+                    maxWidth = 650.px
+                    margin(40.px, LinearDimension.auto)
+                    padding(0.px, 10.px)
+                    fontSize = 18.px / 1.5
+                    fontFamily = """
+                        -apple-system, BlinkMacSystemFont,
+                        'avenir next', avenir,
+                        'Segoe UI',
+                        'lucida grande',
+                        'helvetica neue', helvetica,
+                        'Fira Sans',
+                        roboto, noto,
+                        'Droid Sans',
+                        cantarell, oxygen, ubuntu,
+                        'franklin gothic medium', 'century gothic',
+                        'Liberation Sans',
+                        sans-serif;
+                        """.trimIndent()
+                    color = Color("#444")
+                }
+            }
+        }
     }
 }
 
+suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
+    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+}
